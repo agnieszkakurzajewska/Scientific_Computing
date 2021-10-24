6b26634b54573c9cbcbfea63f56f2fed64cb637a
agnieszkakurzajewska <agnieszkakurzajewska@hotmail.com>
2021-10-24 16:38:07 +0200

    task 2 and 3 done

--- 1/src/2.jl
+++ 1/src/2.jl
@@ -8,7 +8,7 @@ function halfKahan()
     hk = Float16(Float16(Float16(Float16(4) / Float16(3)) - Float16(1)) * 3) - Float16(1)
     hm = eps(Float16)
 
-    println("Float16: obliczone z wyrazenia: ", hk, ", obliczone przez funkcję eps(): ", hm)
+    println("Float16: Calculated from expression: ", hk, ", calculated by  eps() function: ", hm)
 
 end
 
@@ -18,7 +18,7 @@ function singleKahan()
     sk = Float32(Float32(Float32(Float32(4) / Float32(3)) - Float32(1)) * 3)-Float32(1)
     sm = eps(Float32)
 
-    println("Float32: obliczone z wyrazenia: ", sk, ", obliczone przez funkcję eps(): ", sm)
+    println("Float32: Calculated from expression: ", sk, ", calculated by  eps() function: ", sm)
 
 end
 
@@ -28,7 +28,7 @@ function doubleKahan()
     dk = Float64(Float64(Float64(Float64(4) / Float64(3))-Float64(1)) * 3)-Float64(1)
     dm = eps(Float64)
 
-    println("Float64: obliczone z wyrazenia: ", dk, ", obliczone przez funkcję eps(): ", dm)
+    println("Float64: Calculated from expression: ", dk, ", calculated by  eps() function: ", dm)
 
 end
 
--- 1/src/3.jl
+++ 1/src/3.jl
@@ -3,23 +3,19 @@
 
 # Zad. 3
 
-#pokazuję pierwsze 5 liczb, więc nie ustawiam górnej wartości
-#Jeśli wartość następnej liczby obliczonej po dodaniu interwału rózni się od wartości nexFloat, drukowane są obie wersje.
-
-
-function lookAtBytes(interval, start) #interwal to załozony dostęp miedzy kolejnymi liczbami a start to liczba, od której zaczynamy liczenie
+function lookAtBytes(interval, start)
     
     nextValue = Float64(start)
 
     for i in 0:5
 
-        str1 = bitstring(Float64(start + i * interval)) #po dodaniu interwału
-        str2 = bitstring(nextValue) #po dodaniu nextfloat
+        str1 = bitstring(Float64(start + i * interval))
+        str2 = bitstring(nextValue)
 
         if Float64(start + i * interval) == Float64(nextValue)
-            println("           ",str1)
+            println("             ",str1)
         else
-            println("Przeskok:  ",str1,"\nNextFloat: ",str2, "\n")
+            println("Interval δ:  ",str1,"\nNextFloat:   ",str2, "\n")
         end
 
         nextValue = nextfloat(Float64(nextValue))
@@ -28,9 +24,9 @@ end
 
 function printValues()
 
-    #dla delty -52
+    #2^(-52)
     interval = Float64((2)^(-52))
-    println("odstęp równy 2^(-52)")
+    println("δ = 2^(-52):")
 
     println("[1,2]")
     lookAtBytes(interval, Float64(1.0))
@@ -41,9 +37,9 @@ function printValues()
     println("[2,4]")
     lookAtBytes(interval, Float64(2.0))
 
-    #dla delty -51
+    #2^(-51)
     interval = Float64((2)^(-51))
-    println("\nodstęp równy 2^(-51)")
+    println("\nδ = 2^(-51):")
 
     println("[1,2]")
     lookAtBytes(interval, Float64(1.0))
@@ -54,9 +50,9 @@ function printValues()
     println("[2,4]")
     lookAtBytes(interval, Float64(2.0))
 
-    #dla delty -53
+    #2^(-53)
     interval = Float64((2)^(-53))
-    println("\nodstęp równy 2^(-53)")
+    println("\nδ = 2^(-53):")
 
     println("[1,2]")
     lookAtBytes(interval, Float64(1.0))

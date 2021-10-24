e52016139a7cc99f867db2da59a6a75ba011c97a
agnieszkakurzajewska <agnieszkakurzajewska@hotmail.com>
2021-10-24 15:47:34 +0200

    task 1 done

--- 1/src/1a.jl
+++ 1/src/1a.jl
@@ -8,12 +8,11 @@ function machepsHalf()
 
   h = Float16(1) #variable initially set to 1 and then iteratively reduced to machepsu value
 
-  while(Float16(1 + h) > Float16(1) && Float16(1 + h / 2) != Float16(1))
+  while Float16(1 + h) > Float16(1) && Float16(1 + h / 2) != Float16(1)
 
     h = Float16(h / 2)
 
   end
-
   println("Float16: Calculated macheps: ", h, ", macheps given by eps() function: ", eps(Float16))
 
 end
@@ -23,7 +22,7 @@ function machepsSingle()
 
   s = Float32(1)
 
-    while(Float32(1 + s) > Float32(1) && Float32(1 + s / 2) != Float32(1))
+    while Float32(1 + s) > Float32(1) && Float32(1 + s / 2) != Float32(1)
 
   s = Float32(s/2)
 
@@ -38,13 +37,13 @@ function machepsDouble()
 
   d = Float64(1)
 
-  while(Float64(1 + d) > Float64(1) && Float64(1 + d / 2) != Float64(1))
+  while Float64(1 + d) > Float64(1) && Float64(1 + d / 2) != Float64(1)
 
     d = Float64(d / 2)
 
   end
 
-  println("Float64: Calculatedliczony macheps: ", d, ", macheps given by eps() function(): ", eps(Float64))
+  println("Float64: Calculated macheps: ", d, ", macheps given by eps() function(): ", eps(Float64))
 
 end
 
